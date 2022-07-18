abfcb6ea3d72096a34abbddbac8b6af0a4ad42c8
Junying Chen <jygchen@gmail.com>
2022-07-19 09:32:17 +1000

    revert main

--- resultsui/src/main.ts
+++ resultsui/src/main.ts
@@ -25,6 +25,5 @@ Chart.register(...registerables);
 // Declare app
 import { createApp } from 'vue';
 import App from './App.vue';
-const app = createApp(App);
+createApp(App).mount('#app');
 //app.use(router);
-app.mount('#app');
