5c3ea9b51a528f67760110a8bca807e21873bb7d
Junying Chen <jygchen@gmail.com>
2022-07-19 08:19:16 +1000

    Revert dependencies

--- resultsui/src/App.vue
+++ resultsui/src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/monthly-count-report">Monthly count report</router-link>
+  </div>
+  <router-view /> -->
   <MonthlyCountReport />
 </template>
 
--- resultsui/src/main.ts
+++ resultsui/src/main.ts
@@ -1,6 +1,30 @@
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
-import { createApp } from 'vue'
-import App from './App.vue'
-createApp(App).mount('#app')
+// Declare routes
+// import { createRouter, createWebHistory } from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld.vue';
+// import MonthlyCountReport from '@/components/MonthlyCountReport.vue';
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'home',
+//         component: HelloWorld
+//     },
+//     {
+//         path: '/monthly-count-report',
+//         name: 'monthly-count-report',
+//         component: MonthlyCountReport
+//     },
+// ]
+// const router = createRouter({
+//     history: createWebHistory(process.env.BASE_URL),
+//     routes
+// });
+
+// Declare app
+import { createApp } from 'vue';
+import App from './App.vue';
+const app = createApp(App);
+//app.use(router);
+app.mount('#app');
