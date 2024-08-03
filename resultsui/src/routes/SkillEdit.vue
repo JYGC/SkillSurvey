29641f2c80853ba1cb50acfbee773b13d0a309eb
Junying Chen <jygchen@gmail.com>
2024-08-03 21:40:03 +1000

    move skilladd and skilledit to composition API

--- resultsui/src/routes/SkillEdit.vue
+++ resultsui/src/routes/SkillEdit.vue
@@ -17,77 +17,69 @@
     </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import SkillView from '@/components/SkillView.vue';
 import { SkillName } from '@/schemas/skills';
-import { defineComponent, reactive } from 'vue';
-import { useRoute } from 'vue-router';
+import { reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-    setup() {
-        let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
-            skillName: {
-                ID: 0,
-                SkillTypeID: 0,
-                SkillType: null,
-                Name: "",
-                IsEnabled: true,
-                SkillNameAliases: []
-            },
-            newAlias: ""
-        });
-        return {
-            skillModalValue
-        };
+let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
+    skillName: {
+        ID: 0,
+        SkillTypeID: 0,
+        SkillType: null,
+        Name: "",
+        IsEnabled: true,
+        SkillNameAliases: []
     },
-    components: {
-        SkillView
-    },
-    created() {
-        fetch(`http://localhost:3000/skill/getbyid?skillid=${ useRoute().params.skillid }`).then(
-            response => response.json()
-        ).then(data => {
-            this.skillModalValue.skillName.ID = data.ID;
-            this.skillModalValue.skillName.SkillTypeID = data.SkillTypeID;
-            this.skillModalValue.skillName.Name = data.Name;
-            this.skillModalValue.skillName.IsEnabled = data.IsEnabled;
-            this.skillModalValue.skillName.SkillNameAliases = data.SkillNameAliases;
-        });
-    },
-    methods: {
-        saveSkill(): void {
-            this.skillModalValue.skillName
-            fetch('http://localhost:3000/skill/save', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(this.skillModalValue.skillName)
-            }).then(response => response.json()).then(json => {
-                console.log(json);
-                this.$router.go(-1);
-            });
+    newAlias: ""
+});
+
+fetch(`http://localhost:3000/skill/getbyid?skillid=${ useRoute().params.skillid }`).then(
+    response => response.json()
+).then(data => {
+    skillModalValue.skillName.ID = data.ID;
+    skillModalValue.skillName.SkillTypeID = data.SkillTypeID;
+    skillModalValue.skillName.Name = data.Name;
+    skillModalValue.skillName.IsEnabled = data.IsEnabled;
+    skillModalValue.skillName.SkillNameAliases = data.SkillNameAliases;
+});
+
+const router = useRouter();
+
+function saveSkill(): void {
+    skillModalValue.skillName
+    fetch('http://localhost:3000/skill/save', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        deleteSkill(): void {
-            fetch('http://localhost:3000/skill/delete', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    ID: this.skillModalValue.skillName.ID
-                })
-            }).then(response => response.json()).then(json => {
-                console.log(json); // if json is not int, throw error
-                this.$router.go(-1);
-            });
+        body: JSON.stringify(skillModalValue.skillName)
+    }).then(response => response.json()).then(json => {
+        console.log(json);
+        router.back();
+    });
+}
+
+function deleteSkill(): void {
+    fetch('http://localhost:3000/skill/delete', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        isSaveBlocked(): boolean {
-            if (this.skillModalValue.newAlias.trim().length > 0) return true;
-            if (this.skillModalValue.skillName.Name.trim().length === 0) return true;
-            if (this.skillModalValue.skillName.SkillTypeID === 0) return true;
-            return false;
-        }
-    }
-})
+        body: JSON.stringify({
+            ID: skillModalValue.skillName.ID
+        })
+    }).then(response => response.json()).then(json => {
+        console.log(json); // if json is not int, throw error
+        router.back();
+    });
+}
+
+function isSaveBlocked(): boolean {
+    if (skillModalValue.newAlias.trim().length > 0) return true;
+    if (skillModalValue.skillName.Name.trim().length === 0) return true;
+    if (skillModalValue.skillName.SkillTypeID === 0) return true;
+    return false;
+}
 </script>
