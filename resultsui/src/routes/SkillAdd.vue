96c1fcfafa3592a21f9a0a5ddacf660dc9caf693
Junying Chen <jygchen@gmail.com>
2024-08-03 21:42:07 +1000

    move some somethings to composition API (#48)
    * template allows one child
    
    * move skilladd and skilledit to composition API

--- resultsui/src/routes/SkillAdd.vue
+++ resultsui/src/routes/SkillAdd.vue
@@ -1,63 +1,57 @@
 <template>
-    <div class="row vertical-padding">
-        <div class="col-md-12">
-            <b-button class="float-start" @click.prevent="$router.go(-1)">Back</b-button>
-            <b-button class="float-end" v-on:click="addNewSKill()"
-              :disabled="isAddBlocked()">Add</b-button>
+    <div>
+        <div class="row vertical-padding">
+            <div class="col-md-12">
+                <b-button class="float-start" @click.prevent="$router.go(-1)">Back</b-button>
+                <b-button class="float-end" v-on:click="addNewSKill()"
+                :disabled="isAddBlocked()">Add</b-button>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <SkillView v-model="skillModalValue" :forSkillTypeID="forSkillTypeID" />
+        <div class="row">
+            <SkillView v-model="skillModalValue" :forSkillTypeID="forSkillTypeID" />
+        </div>
     </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import SkillView from '@/components/SkillView.vue';
 import { SkillName } from '@/schemas/skills';
-import { defineComponent, reactive } from 'vue';
-import { useRoute } from 'vue-router';
+import { reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-    setup() {
-        let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
-            skillName: {
-                ID: 0,
-                SkillTypeID: 0,
-                SkillType: null,
-                Name: "",
-                IsEnabled: true,
-                SkillNameAliases: []
-            },
-            newAlias: ""
-        });
-        let forSkillTypeID = useRoute().params.skilltypeid;
-        return {
-            skillModalValue,
-            forSkillTypeID
-        };
+let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
+    skillName: {
+        ID: 0,
+        SkillTypeID: 0,
+        SkillType: null,
+        Name: "",
+        IsEnabled: true,
+        SkillNameAliases: []
     },
-    components: {
-        SkillView
-    },
-    methods: {
-        addNewSKill(): void {
-            fetch('http://localhost:3000/skill/add', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(this.skillModalValue.skillName)
-            }).then(response => response.json()).then(json => {
-                console.log(json);
-                this.$router.go(-1);
-            });
+    newAlias: ""
+});
+
+let forSkillTypeID = useRoute().params.skilltypeid;
+
+const router = useRouter();
+
+function addNewSKill(): void {
+    fetch('http://localhost:3000/skill/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        isAddBlocked(): boolean {
-            if (this.skillModalValue.newAlias.trim().length > 0) return true;
-            if (this.skillModalValue.skillName.Name.trim().length === 0) return true;
-            if (this.skillModalValue.skillName.SkillTypeID === 0) return true;
-            return false;
-        }
-    }
-})
+        body: JSON.stringify(skillModalValue.skillName)
+    }).then(response => response.json()).then(json => {
+        console.log(json);
+        router.back();
+    });
+}
+
+function isAddBlocked(): boolean {
+    if (skillModalValue.newAlias.trim().length > 0) return true;
+    if (skillModalValue.skillName.Name.trim().length === 0) return true;
+    if (skillModalValue.skillName.SkillTypeID === 0) return true;
+    return false;
+}
 </script>
--- resultsui/src/routes/SkillEdit.vue
+++ resultsui/src/routes/SkillEdit.vue
@@ -1,91 +1,85 @@
 <template>
-    <div class="row vertical-padding">
-        <div class="col-md-12">
-            <b-button class="float-start" @click.prevent="$router.go(-1)">Back</b-button>
-            <b-button class="float-end margin-left-10" v-on:click="saveSkill()"
-              :disabled="isSaveBlocked()">Save</b-button>
-            <b-button class="float-end" v-b-modal.confirm-delete>Delete</b-button>
+    <div>
+        <div class="row vertical-padding">
+            <div class="col-md-12">
+                <b-button class="float-start" @click.prevent="$router.go(-1)">Back</b-button>
+                <b-button class="float-end margin-left-10" v-on:click="saveSkill()"
+                :disabled="isSaveBlocked()">Save</b-button>
+                <b-button class="float-end" v-b-modal.confirm-delete>Delete</b-button>
+            </div>
         </div>
+        <div class="row">
+            <SkillView v-model="skillModalValue" />
+        </div>
+        <b-modal id="confirm-delete" hide-header ok-title="Confirm" ok-variant="danger" @ok="deleteSkill()">
+            <p>Are you sure you want to delete this skill?</p>
+        </b-modal>
     </div>
-    <div class="row">
-        <SkillView v-model="skillModalValue" />
-    </div>
-    <b-modal id="confirm-delete" hide-header ok-title="Confirm" ok-variant="danger" @ok="deleteSkill()">
-        <p>Are you sure you want to delete this skill?</p>
-    </b-modal>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import SkillView from '@/components/SkillView.vue';
 import { SkillName } from '@/schemas/skills';
-import { defineComponent, reactive } from 'vue';
-import { useRoute } from 'vue-router';
+import { reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-    setup() {
-        let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
-            skillName: {
-                ID: 0,
-                SkillTypeID: 0,
-                SkillType: null,
-                Name: "",
-                IsEnabled: true,
-                SkillNameAliases: []
-            },
-            newAlias: ""
-        });
-        return {
-            skillModalValue
-        };
+let skillModalValue: { skillName: SkillName, newAlias: string } = reactive({
+    skillName: {
+        ID: 0,
+        SkillTypeID: 0,
+        SkillType: null,
+        Name: "",
+        IsEnabled: true,
+        SkillNameAliases: []
     },
-    components: {
-        SkillView
-    },
-    created() {
-        fetch(`http://localhost:3000/skill/getbyid?skillid=${ useRoute().params.skillid }`).then(
-            response => response.json()
-        ).then(data => {
-            this.skillModalValue.skillName.ID = data.ID;
-            this.skillModalValue.skillName.SkillTypeID = data.SkillTypeID;
-            this.skillModalValue.skillName.Name = data.Name;
-            this.skillModalValue.skillName.IsEnabled = data.IsEnabled;
-            this.skillModalValue.skillName.SkillNameAliases = data.SkillNameAliases;
-        });
-    },
-    methods: {
-        saveSkill(): void {
-            this.skillModalValue.skillName
-            fetch('http://localhost:3000/skill/save', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(this.skillModalValue.skillName)
-            }).then(response => response.json()).then(json => {
-                console.log(json);
-                this.$router.go(-1);
-            });
+    newAlias: ""
+});
+
+fetch(`http://localhost:3000/skill/getbyid?skillid=${ useRoute().params.skillid }`).then(
+    response => response.json()
+).then(data => {
+    skillModalValue.skillName.ID = data.ID;
+    skillModalValue.skillName.SkillTypeID = data.SkillTypeID;
+    skillModalValue.skillName.Name = data.Name;
+    skillModalValue.skillName.IsEnabled = data.IsEnabled;
+    skillModalValue.skillName.SkillNameAliases = data.SkillNameAliases;
+});
+
+const router = useRouter();
+
+function saveSkill(): void {
+    skillModalValue.skillName
+    fetch('http://localhost:3000/skill/save', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        deleteSkill(): void {
-            fetch('http://localhost:3000/skill/delete', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    ID: this.skillModalValue.skillName.ID
-                })
-            }).then(response => response.json()).then(json => {
-                console.log(json); // if json is not int, throw error
-                this.$router.go(-1);
-            });
+        body: JSON.stringify(skillModalValue.skillName)
+    }).then(response => response.json()).then(json => {
+        console.log(json);
+        router.back();
+    });
+}
+
+function deleteSkill(): void {
+    fetch('http://localhost:3000/skill/delete', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        isSaveBlocked(): boolean {
-            if (this.skillModalValue.newAlias.trim().length > 0) return true;
-            if (this.skillModalValue.skillName.Name.trim().length === 0) return true;
-            if (this.skillModalValue.skillName.SkillTypeID === 0) return true;
-            return false;
-        }
-    }
-})
+        body: JSON.stringify({
+            ID: skillModalValue.skillName.ID
+        })
+    }).then(response => response.json()).then(json => {
+        console.log(json); // if json is not int, throw error
+        router.back();
+    });
+}
+
+function isSaveBlocked(): boolean {
+    if (skillModalValue.newAlias.trim().length > 0) return true;
+    if (skillModalValue.skillName.Name.trim().length === 0) return true;
+    if (skillModalValue.skillName.SkillTypeID === 0) return true;
+    return false;
+}
 </script>
